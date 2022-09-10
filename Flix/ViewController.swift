01e4f6030ad25f7a24db1e406edcc1dfd4915b9c
Sherry Liu <2506-sl3689@users.noreply.gitlab.cci.drexel.edu>
2022-09-10 16:29:27 -0400

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Sherry Liu on 9/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
