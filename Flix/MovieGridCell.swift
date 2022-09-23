03f18c05aea4567edc9f7c402a2a6567f6c35d3c
sherrss <liu1500sherry@gmail.com>
2022-09-23 12:54:14 -0400

    Add Flix Part 2 files

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Sherry Liu on 9/23/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
